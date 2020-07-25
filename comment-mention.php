37fa9d9b59235b7687e334775bc2edad6a50408b
Bhargav Bhandari <bhargav@buddyboss.com>
2020-07-25 14:34:13 +0530

    Set plugin admin notice and setting page link

--- comment-mention.php
+++ comment-mention.php
@@ -43,6 +43,112 @@ if ( ! defined( 'CMT_MNTN_BASE_NAME' ) ) {
 }
 
 /**
+ * Setting link for plugin.
+ *
+ * @param  array $links Array of plugin setting link.
+ * @return array
+ */
+function cmt_mntn_setting_page_link( $links ) {
+
+	$settings_link = sprintf(
+		'<a href="%1$s">%2$s</a>',
+		esc_url( admin_url( 'admin.php?page=comment-mention' ) ),
+		esc_html__( 'Settings', 'comment-mention' )
+	);
+
+	array_push( $links, $settings_link );
+	return $links;
+}
+
+add_filter( 'plugin_action_links_' . CMT_MNTN_BASE_NAME, 'cmt_mntn_setting_page_link' );
+
+/**
+ * Show admin notice.
+ *
+ * @return void
+ */
+function sample_admin_notice__success() {
+
+	// Check if notice is dismissed.
+	$notice_dissmissed = get_option( 'dismiss-cm-notice' );
+
+	// If notice is dismissed, then don't display it.
+	if ( ! empty( $notice_dissmissed ) && 1 == $notice_dissmissed ) {
+		return;
+	}
+
+	?>
+	<div class="notice notice-success is-dismissible">
+		<?php
+		echo sprintf(
+			'<p>%1$s</p><a href="%2$s" target="_blank">%3$s</a> | <a href="%4$s">%5$s</a>',
+			esc_html__( 'Do you like Comment Mention plugin? Support it by buying me coffee OR beer. ;)', 'comment-mention' ),
+			'https://www.paypal.com/paypalme/BnB90/',
+			esc_html__( 'Buy Coffee/Beer', 'comment-mention' ),
+			esc_url( wp_nonce_url( add_query_arg( 'dismiss-cm-notice', '1' ), 'dismiss-cm-notice-' . get_current_user_id() ) ),
+			esc_html__( 'Dismiss this notice', 'comment-mention' )
+		);
+		?>
+	</div>
+	<?php
+}
+
+add_action( 'admin_notices', 'sample_admin_notice__success' );
+
+/**
+ * Register dismissal of admin notices.
+ */
+function cmt_mntn_dismiss() {
+	if ( isset( $_GET['dismiss-cm-notice'] ) && check_admin_referer( 'dismiss-cm-notice-' . get_current_user_id() ) ) {
+		update_option( 'dismiss-cm-notice', 1 );
+	}
+}
+
+add_action( 'admin_init', 'cmt_mntn_dismiss' );
+
+/**
+ * Reset admin notice on plugin upgrade.
+ *
+ * @param  object $upgrader_object Upgrade object.
+ * @param  array  $options         Array of plugin related data.
+ * @return void
+ */
+function cmt_mntn_upgrade_function( $upgrader_object, $options ) {
+	$current_plugin_path_name = plugin_basename( __FILE__ );
+
+	if ( $options['action'] == 'update' && $options['type'] == 'plugin' ) {
+		foreach ( $options['plugins'] as $each_plugin ) {
+			if ( $each_plugin == $current_plugin_path_name ) {
+
+				$notice_dissmissed = get_option( 'dismiss-cm-notice' );
+				if ( ! empty( $notice_dissmissed ) ) {
+					delete_option( 'dismiss-cm-notice' );
+				}
+			}
+		}
+	}
+}
+
+add_action( 'upgrader_process_complete', 'cmt_mntn_upgrade_function', 10, 2 );
+
+/**
+ * Reset option to show admin notice.
+ */
+function cmt_mntn_deactivation_callback() {
+
+	// Check if notice is dismisses.
+	$notice_dissmissed = get_option( 'dismiss-cm-notice' );
+
+	// If dismissed, then reset to show when plugin is re-activate.
+	if ( ! empty( $notice_dissmissed ) ) {
+		delete_option( 'dismiss-cm-notice' );
+	}
+}
+
+register_deactivation_hook( __FILE__, 'cmt_mntn_deactivation_callback' );
+register_activation_hook( __FILE__, 'cmt_mntn_deactivation_callback' );
+
+/**
  * Apply transaltion file as per WP language.
  */
 function cmt_mntn_text_domain_loader() {
