c33810332716760f7adbfd45614d32dcfa0dccf2
Jay Patel <pjay9981@gmail.com>
2024-02-15 13:36:18 +0530

    Version updated to v1.7.6

--- comment-mention.php
+++ comment-mention.php
@@ -6,7 +6,7 @@
  * Author URI:      https://bhargavb.com
  * Text Domain:     comment-mention
  * Domain Path:     /languages
- * Version:         1.7.5
+ * Version:         1.7.6
  *
  * @package         Comment_Mention
  */
@@ -21,7 +21,7 @@ if ( ! defined( 'CMT_MNTN_VERSION' ) ) {
 	/**
 	 * The version of the plugin.
 	 */
-	define( 'CMT_MNTN_VERSION', '1.7.5' );
+	define( 'CMT_MNTN_VERSION', '1.7.6' );
 }
 if ( ! defined( 'CMT_MNTN_PATH' ) ) {
 	/**
