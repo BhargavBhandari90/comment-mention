657f8f5e44590daf684d36e874ee6bb8b6654ef4
Hackkzy <pjay9981@gmail.com>
2024-03-13 13:45:43 +0530

    Version updated

--- comment-mention.php
+++ comment-mention.php
@@ -6,7 +6,7 @@
  * Author URI:      https://bhargavb.com
  * Text Domain:     comment-mention
  * Domain Path:     /languages
- * Version:         1.7.9
+ * Version:         1.7.10
  *
  * @package         Comment_Mention
  */
@@ -21,7 +21,7 @@ if ( ! defined( 'CMT_MNTN_VERSION' ) ) {
 	/**
 	 * The version of the plugin.
 	 */
-	define( 'CMT_MNTN_VERSION', '1.7.9' );
+	define( 'CMT_MNTN_VERSION', '1.7.10' );
 }
 if ( ! defined( 'CMT_MNTN_PATH' ) ) {
 	/**
