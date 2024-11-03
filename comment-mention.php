0228b5d0261b0baa6bb4665c280184183d013696
Bunty <bn.bhandari90@gmail.com>
2024-11-03 18:59:42 +0530

    Update to v1.7.12

--- comment-mention.php
+++ comment-mention.php
@@ -6,7 +6,7 @@
  * Author URI:      https://bhargavb.com
  * Text Domain:     comment-mention
  * Domain Path:     /languages
- * Version:         1.7.11
+ * Version:         1.7.12
  *
  * @package         Comment_Mention
  */
@@ -21,7 +21,7 @@ if ( ! defined( 'CMT_MNTN_VERSION' ) ) {
 	/**
 	 * The version of the plugin.
 	 */
-	define( 'CMT_MNTN_VERSION', '1.7.11' );
+	define( 'CMT_MNTN_VERSION', '1.7.12' );
 }
 if ( ! defined( 'CMT_MNTN_PATH' ) ) {
 	/**
