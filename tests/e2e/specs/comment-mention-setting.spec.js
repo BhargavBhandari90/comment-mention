16460748a296f0c212c9f359a172a27dad96d0c3
Bunty <bn.bhandari90@gmail.com>
2025-06-05 17:56:55 +0530

    e2e test improvement

--- tests/e2e/specs/comment-mention-setting.spec.js
+++ tests/e2e/specs/comment-mention-setting.spec.js
@@ -4,10 +4,16 @@ test.use( { storageState: process.env.ADMINSTATE } );
 
 test( 'Check Plugin Setting Page', async ( { page } ) => {
     await page.goto('/wp-admin/');
+
     await page.getByRole('link', { name: 'Comment Mention' }).click();
     await expect(page.getByRole('heading', { name: 'Comment Mention' })).toBeVisible();
+
     await page.getByRole('cell', { name: 'Choose which user roles' }).getByLabel('Subscriber').check();
+    await expect(page.getByRole('cell', { name: 'Choose which user roles' }).getByLabel('Subscriber')).toBeChecked();
+
     await page.locator('input[name="cmt_mntn_email_enable"]').check();
+    await expect(page.locator('input[name="cmt_mntn_email_enable"]')).toBeChecked();
+
     await page.locator('#submit').click();
     await expect(page.getByText('Settings Saved')).toBeVisible();
 });
--- tests/e2e/specs/comment-mention.spec.js
+++ tests/e2e/specs/comment-mention.spec.js
@@ -4,18 +4,17 @@ test.use( { storageState: process.env.ADMINSTATE } );
 
 test( 'Mention users in Post Comment', async ( { page } ) => {
     await page.goto('/hello-world');
+
     await page.getByRole('textbox', { name: 'Comment *' }).click();
     await page.getByRole('textbox', { name: 'Comment *' }).pressSequentially('@test',{ delay: 200 });
 
+    // Wait for the mention suggestion to appear.
     const mentionItem = page.locator('.tribute-container li.highlight');
-
-    // Wait for the mention suggestion to appear
     await expect(mentionItem).toBeVisible({ timeout: 3000 });
     await expect(mentionItem).toContainText('testuser');
 
     await mentionItem.click();
 
-    // await page.getByText('testuser (testuser)').click();
     await page.getByRole('textbox', { name: 'Comment *' }).pressSequentially(' this is test comment',{ delay: 100 });
     await page.getByRole('button', { name: 'Post Comment' }).click();
     await expect(page.getByText('@testuser this is test comment').last()).toBeVisible({ timeout: 10000 });
